179f9230eb08b5f252d1e411c72610b002eeb3da
stephenjacobsio <contact@stephenjacobs.io>
2025-01-20 18:29:28 -0800

    feat: implement settings module
    Add `settings.py` to manage configuration constants:
    - Define supported video file extensions and default settings.
    
    Signed-off-by: stephenjacobsio <contact@stephenjacobs.io>

---
+++ src/config/settings.py
@@ -0,0 +1,8 @@
+# Supported video file extensions
+VIDEO_EXTENSIONS = (
+    ".mp4", ".mov", ".mkv", ".avi",
+    ".flv", ".wmv", ".m4v", ".ts", ".webm"
+)
+
+# Default logging level
+DEFAULT_LOG_LEVEL = "INFO"
